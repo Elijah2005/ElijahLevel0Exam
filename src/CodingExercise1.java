f7a8fb0499c4c8b395f29e25184c121df7224310
gerings17 <skyswims@yahoo.com>
2017-01-02 17:47:29 -0800

    finished level 0 exam

--- src/CodingExercise1.java
+++ src/CodingExercise1.java
@@ -1,36 +1,39 @@
-/* Level 0 Exam: Coding  Exercise #1 */
 import java.awt.Color;
-
 import javax.swing.JOptionPane;
-import org.teachingextensions.logo.Colors;
-import org.teachingextensions.logo.Tortoise;
+import org.jointheleague.graphical.robot.Robot;
 
 
-public class CodingExercise1
-{
-	public static void main(String[] args)
-	{
-		// 3. ask the user what color they would like the tortoise to draw
-		
-		// 4. use an if/else statement to set the pen color that the user requested (minimum of 2 colors)
-		
-		// 2. set the pen width to 10
-		
-		// 1. make the tortoise draw a shape
-		JOptionPane.showInputDialog("What color do you want me to draw");
-		
-			
-		
-		Tortoise.penDown();
-		Tortoise.setPenColor(Color.RED);
-		Tortoise.setPenWidth(100);
-		Tortoise.setSpeed(10);
-		Tortoise.move(40);
-Tortoise.turn(90);
-Tortoise.move(40);
-Tortoise.turn(90);
-Tortoise.move(40);
-Tortoise.turn(90);
-Tortoise.move(40);
-	}
+/* Level 0 Exam: Coding  Exercise #1 */
+public class CodingExercise1 {
+
+
+    public static void main(String[] args) {
+   	 // 3. ask the user what color they would like the Robot to draw
+
+
+   	 // 4. use an if/else statement to set the pen color that the user requested (minimum of 2 colors)
+
+
+   	 // 2. set the pen width to 10
+
+
+   	 // 1. make the Robot draw a shape
+   	 Robot E = new Robot();
+   	 JOptionPane.showMessageDialog(null, "The color choices are blue and red");
+   String color =	 JOptionPane.showInputDialog("What color would you like the robot to draw");
+   if(color.equals("blue")){
+	   E.setPenColor(Color.BLUE);
+   } else if(color.equals("red")) {
+	   E.setPenColor(Color.RED);
+   }
+   	 E.setPenWidth(10);
+   	 E.setSpeed(10);
+   	 for (int i = 0; i < 4; i++) {
+   	 E.turn(90);
+   	 E.penDown();
+   	 E.move(200);
+   	 }
+    }
+
+
 }
